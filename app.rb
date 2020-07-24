f5e1e9c3ed2e55d7965993de5984240c6bc831ed
Jonathan East <jonathan_east@ymail.com>
2020-07-24 01:12:55 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,7 +8,13 @@ class App < Sinatra::Base
   end
   
   post '/team' do
-    @name = params[]
+    @name = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @sf = params[:sf]
+    @pf = params[:pf]
+    @c = params[:c]
     erb :team
   end
 
